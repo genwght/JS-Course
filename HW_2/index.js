0a84c7290d1eff4d80283ae6165c07c66b271a06
genwght <wmelnyk88@gmail.com>
2025-02-05 16:19:08 +0200

    add new command

--- HW_2/index.js
+++ HW_2/index.js
@@ -3,4 +3,6 @@ secondElement = prompt('Enter your favorite fruit');
 thirdElement = prompt('Enter your favorite film');
 let resultOfUrAnswer = [firstElement, secondElement, thirdElement]; 
 resultOfUrAnswer = alert(`${firstElement}, ${secondElement}, ${thirdElement}`);
-
+console.log(firstElement.concat(' ', secondElement));
+console.log(secondElement.concat(' ', firstElement));
+console.log(firstElement.concat(' ', thirdElement));
