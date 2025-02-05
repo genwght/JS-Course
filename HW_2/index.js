b9937f2ae7338f1c8f8563aace8d8b7d742b71c0
genwght <wmelnyk88@gmail.com>
2025-02-05 15:26:47 +0200

    make new changes

--- HW_2/index.js
+++ HW_2/index.js
@@ -2,4 +2,5 @@ firstElement = prompt('Enter your favorite animal');
 secondElement = prompt('Enter your favorite fruit');
 thirdElement = prompt('Enter your favorite film');
 let strings = [firstElement, secondElement, thirdElement]; 
+strings.sort(() => Math.random() - 0.5);
 resultOfUrAnswer = alert(`${firstElement}, ${secondElement}, ${thirdElement}`);
