66f757d995908d0ee4953d8fd320b6477f697dde
genwght <wmelnyk88@gmail.com>
2025-02-05 15:05:41 +0200

    add new element

--- HW_2/index.js
+++ HW_2/index.js
@@ -1,1 +1,4 @@
-prompt('Enter your three favorite animals')
+firtElement = prompt('Enter your favorite animal');
+secondElement = prompt('Enter your favorite fruit');
+thirdElement = prompt('Enter your favorite film');
+let strings = [firtElement, secondElement, thirdElement]; 
