475f41566b3f8b2af978270a62e53aa99927cc09
genwght <wmelnyk88@gmail.com>
2025-02-05 17:06:26 +0200

    added new finctions to index.js

--- HW_2/index.js
+++ HW_2/index.js
@@ -1,10 +1,15 @@
-let strings = [prompt('Enter your favorite animal'), prompt('Enter your favorite fruit'), prompt('Enter your favorite film')];
-
-// Перемішуємо масив
+let strings = [
+    prompt('Enter your favorite animal'),
+    prompt('Enter your favorite fruit'),
+    prompt('Enter your favorite film')
+];
 strings.sort(() => Math.random() - 0.5);
-
-// Об'єднуємо у рядок
 let result = strings.join(" ");
 console.log(result);
 alert(result);
+
+userCod = prompt('Enter five-digit number');
+console.log(typeof 0);
+userCodResult = userCod.split(" ");
+alert(userCodResult);
 
