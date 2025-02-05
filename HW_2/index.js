ad1cd72715d5ecd6733dfcf0bd58f5e41544c13c
genwght <wmelnyk88@gmail.com>
2025-02-05 15:11:57 +0200

    deleted files

--- HW_2/index.js
+++
@@ -1,4 +0,0 @@
-firtElement = prompt('Enter your favorite animal');
-secondElement = prompt('Enter your favorite fruit');
-thirdElement = prompt('Enter your favorite film');
-let strings = [firtElement, secondElement, thirdElement]; 
