973487c6cd049a395db6d441fb0d2bf899b57fd0
genwght <wmelnyk88@gmail.com>
2025-02-05 15:53:54 +0200

    delete not work element

--- HW_2/index.js
+++ HW_2/index.js
@@ -2,7 +2,5 @@ firstElement = prompt('Enter your favorite animal');
 secondElement = prompt('Enter your favorite fruit');
 thirdElement = prompt('Enter your favorite film');
 let resultOfUrAnswer = [firstElement, secondElement, thirdElement]; 
-resultOfUrAnswer.sort(() => Math.random() - 0.5);
-resultOfUrAnswer = alert(join(" "));
-console.log(resultOfUrAnswer.join(" "));
+resultOfUrAnswer = alert(`${firstElement}, ${secondElement}, ${thirdElement}`);
 
