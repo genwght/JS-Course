9e521e2d7a3f79306e3bf96773361b38f72914a6
genwght <wmelnyk88@gmail.com>
2025-02-06 12:18:23 +0200

    edit index2.js

--- HW_2/index2.js
+++ HW_2/index2.js
@@ -1,8 +1,5 @@
 userNumber1 = prompt('Please enter first number');
 userNumber2 = prompt('Please enter second number');
 console.log (typeof 0);
-let additional = userNumber1 + userNumber2;
-let subtraction = userNumber1 - userNumber2;
-let multiplication = userNumber1 * userNumber2;
-let division = userNumber2 !== 0 ? userNumber1 / userNumber2 :
-results = alert(`${additional}, ${subtraction}, ${multiplication}, ${division}`);
+additon = parseInt(userNumber1) + parseInt (userNumber2);
+console.log(additon);
