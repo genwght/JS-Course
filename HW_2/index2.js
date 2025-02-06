623f997d3a325d4da2f0c558b94b962dd5f03b58
genwght <wmelnyk88@gmail.com>
2025-02-06 12:40:05 +0200

    remove not working element

--- HW_2/index2.js
+++ HW_2/index2.js
@@ -1,6 +1,5 @@
 userNumber1 = prompt('Please enter first number');
 userNumber2 = prompt('Please enter second number');
-console.log (typeof 0);
 additon = parseInt(userNumber1) + parseInt (userNumber2);
 console.log(additon);
 subtraction = parseInt(userNumber1) - parseInt (userNumber2);
