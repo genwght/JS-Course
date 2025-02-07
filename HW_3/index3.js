9dfa68aa161641b1f97d4c80c9d3f41d303d0d7f
genwght <wmelnyk88@gmail.com>
2025-02-07 23:24:52 +0200

    add first part of switch boc

---
+++ HW_3/index3.js
@@ -0,0 +1,16 @@
+userInput = prompt("Enter a value:");
+
+// if (userInput === null) {
+//     console.log("Input was canceled.");
+// } else if (userInput.trim() === "") {
+//     console.log("You entered an empty string.");
+// } else if (isNaN(+userInput)) {
+//     console.log("This is not a number.");
+// } else {
+//     console.log("Valid number entered!");
+// }
+
+switch (userInput) {
+    case 'null' :
+        newUserInput = 'Input was canceled.' ;
+}
