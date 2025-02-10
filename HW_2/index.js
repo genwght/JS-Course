42a901a8b651302ff31416c76327f37c50806328
genwght <wmelnyk88@gmail.com>
2025-02-10 10:47:53 +0200

    restore deleted file

--- HW_2/index.js
+++
@@ -1,9 +0,0 @@
-userPet = prompt('Enter your favorite animal');
-userFruit = prompt('Enter your favorite fruit');
-userFilm = prompt('Enter your favorite film');
-
-alert(`${userFilm},${userPet},${userFruit}`)
-
-userNum = prompt('Please, enter five-digit number');
-userResult = userNum.split(' ');
-alert(userResult);
