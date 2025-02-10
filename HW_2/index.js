fd764675a69f3504b319e73336c714189bffc37b
genwght <wmelnyk88@gmail.com>
2025-02-10 10:48:27 +0200

    added all last part

---
+++ HW_2/index.js
@@ -0,0 +1,8 @@
+userPet = prompt('Enter your favorite animal');
+userFruit = prompt('Enter your favorite fruit');
+userFilm = prompt('Enter your favorite film');
+
+alert(`${userFilm}, ${userPet}, ${userFruit}`);
+
+userNum = prompt('Please, enter five-digit number');
+userResult = userNum.replaceAll('', ' ');
