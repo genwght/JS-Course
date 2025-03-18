eb03425f59b56fce25983b09a87a6302a8999efb
genwght <wmelnyk88@gmail.com>
2025-03-18 20:58:11 +0200

    all is done

--- HW_15_03/index.js
+++ HW_15_03/index.js
@@ -12,8 +12,34 @@
 // 1️⃣ Clicked “Start Color Change” → The background color changes every 2 seconds.
 // 2️⃣ Clicked “Stop Color Change” → The color change stops.
 
-const div = document.querySelector('.wrapp');
+const myInterval = setInterval(() => {
+     setColorWithInterval(div);
+}, 2000);
 
-div.onclick = () => {
+function getRandomNumber() {
+    return Math.round(Math.random() * (255 - 0) + 0);
+}
+
+function getRandomColor() {
+    return `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
+};
+
+function setColorWithInterval() {
+    div.style.backgroundColor = getRandomColor();
+}
+
+const btnStart = document.querySelector('.start');
+const div = document.querySelector('.desktop-color');
+
+btnStart.onclick = () => {
     console.log('it work');
+    setInterval(myInterval);
+};
+
+
+const btnStop = document.querySelector('.stop');
+
+btnStop.onclick = () => {
+    console.log('it stop');
+    clearInterval(myInterval);
 };
